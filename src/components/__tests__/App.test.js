75bb5cc511b03d7d750f3ebffeb5ef090460071d
Quan Truong <qmtruong92@gmail.com>
2018-06-08 02:57:36 -0700

    + Fixed indentation

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -15,24 +15,24 @@ beforeEach(() => {
 // Ensure that an instance of CommentBox is within the App component
 it('shows a comment box', () => {
 
-	/*
-		const div = document.createElement('div');
+/*
+	const div = document.createElement('div');
 
-		ReactDOM.render(<App />, div);
+	ReactDOM.render(<App />, div);
 
-		// Expectation: The inner html of the div contains the string
-		// 'The comment box component'
-		// WRONG. accessing a component's properties from the parent component, App
-		// expect(div.innerHTML).toContain('The comment box component');
-		
-		
-		// RIGHT somethign like this, where we can verify that the parent
-		// component contains an instance of CommentBox as a child
-		// expect(div).toHaveAnInstanceOf(CommentBox);
-		
+	// Expectation: The inner html of the div contains the string
+	// 'The comment box component'
+	// WRONG. accessing a component's properties from the parent component, App
+	// expect(div.innerHTML).toContain('The comment box component');
+	
+	
+	// RIGHT somethign like this, where we can verify that the parent
+	// component contains an instance of CommentBox as a child
+	// expect(div).toHaveAnInstanceOf(CommentBox);
+	
 
-		ReactDOM.unmountComponentAtNode(div);
-	*/
+	ReactDOM.unmountComponentAtNode(div);
+*/
 
 	// found a CommentBox instance
 	// find returns an array
