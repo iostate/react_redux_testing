0eee157f073916038da0c5dff61b6a23dd05fc1f
Quan Truong <qmtruong92@gmail.com>
2018-06-08 02:59:59 -0700

    + Save comment type, comment reducer

---
+++ src/reducers/comments.js
@@ -0,0 +1,15 @@
+import { SAVE_COMMENT } from 'actions/types';
+
+/**
+ * 
+ * @param {*} state Array of Strings (since the comments are Strings)
+ * @param {*} action 
+ */
+export default function(state = [], action) {
+	switch (action.type) {
+		case SAVE_COMMENT:
+			return [...sate, action.payload];
+		default:
+			return state;
+	}
+}
