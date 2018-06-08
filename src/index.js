f0bacb16862c8a717ebfe286737671c0a0db97cb
Quan Truong <qmtruong92@gmail.com>
2018-06-08 02:54:32 -0700

    + Save Comment action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+	<Provider store={createStore(reducers, {})}>
+		<App />
+	</Provider>
+	, document.querySelector('#root')
+);
