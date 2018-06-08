3485986e73b5934f614d8da12353d80e6991279b
Quan Truong <qmtruong92@gmail.com>
2018-06-07 17:52:06 -0700

    Add event handlers

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+	state = { comment: ''};
+
+	// the folowing syntax is called a bound arrow function
+	handleChange = (event) => {
+		this.setState({comment: event.target.value});
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+
+		// TODO - Call an action creator
+		// And save the comment
+
+		// Reset the input
+		this.setState({comment: ''});
+
+	}
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
 				<h4>Add a Comment</h4>
-				<textarea />
+				<textarea 
+					onChange={this.handleChange}
+					value={this.state.comment} 
+				/>
 				<div>
 					<button>Submit Comment</button>
 				</div>
