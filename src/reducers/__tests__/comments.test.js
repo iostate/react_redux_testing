265fb777c2bc7a1f7a4a8874c2b3072a5d826fcd
Quan Truong <qmtruong92@gmail.com>
2018-06-10 13:20:21 -0700

    Added test to ensure action throws no error when undefined action given to reducer

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -1,5 +1,6 @@
 import commentsReducer from 'reducers/comments';
 import { SAVE_COMMENT } from 'actions/types';
+import comments from '../comments';
 
 it('handles actions of type SAVE_COMMENT', () => {
 	// Create a fake action to push 
@@ -13,4 +14,13 @@ it('handles actions of type SAVE_COMMENT', () => {
 
 	// test new state
 	expect(newState).toEqual(['New Comment']);
+});
+
+// Author does not believe that writing tests for 
+// unknown action types is useful. Believes it is 
+// more useful to write tests that the reducer is 
+// more likely to actually encounter.
+it('handles action with unknown type', () => {
+	const newState = commentsReducer([], {type: 'ASDASDDAS'});
+	expect(newState).toEqual([]);
 });
