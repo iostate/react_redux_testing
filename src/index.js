28c9bbb1ddaf700f0a49a06a01870ac145a67193
Quan Truong <qmtruong92@gmail.com>
2018-06-10 11:30:39 -0700

    Created helper fn to allow adding Provider w/ store to components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from 'reducers';
+import Root from 'Root';
+/*
+
+	Following import lines have been replaced with function from
+	Root.js
+
+	// import { Provider } from 'react-redux';
+	// import { createStore, applyMiddleware } from 'redux';
+	// import reducers from 'reducers';
+*/
+
 import App from 'components/App';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 
 ReactDOM.render(
-	<Provider store={createStore(reducers, {})}>
+	<Root>
 		<App />
-	</Provider>
+	</Root>
 	, document.querySelector('#root')
 ); 
