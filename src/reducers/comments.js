7cafe84de5ff6a5c5cd878b2d6df6c20b871c5a4
Quan Truong <qmtruong92@gmail.com>
2018-06-10 10:59:16 -0700

    Pre-Bonding React with Redux

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -8,7 +8,7 @@ import { SAVE_COMMENT } from 'actions/types';
 export default function(state = [], action) {
 	switch (action.type) {
 		case SAVE_COMMENT:
-			return [...sate, action.payload];
+			return [...state, action.payload];
 		default:
 			return state;
 	}
